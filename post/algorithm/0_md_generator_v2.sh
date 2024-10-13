9111c034af28909e294219aaeb1908acdf7cb738
li54426 <li54426@users.noreply.github.com>
2024-10-13 13:16:12 +0000

    auto deploy li54426/Blog@8c6508b45970bd2ba441822f7917024de27eb5a0

---
+++ post/algorithm/0_md_generator_v2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+filename=$(date +"%Y-%m-%d-.md")
+
+cat > "$filename" << EOF
+---
+layout: blog
+banana: true
+category: algorithm
+title:  
+date:   $(date +"%Y-%m-%d %H:%M:%S")
+background: purple
+tags:
+- algorithm
+---
+
+
+EOF
+
+echo "文件已生成：$filename"
