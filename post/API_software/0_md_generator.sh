9332d3c5d065b198f1f673bf170b67b282c2fe34
li54426 <li54426@users.noreply.github.com>
2024-10-13 07:27:08 +0000

    auto deploy li54426/Blog@0f69f9dd81c6daf1695a8f6d7766f78e28551b65

---
+++ post/API_software/0_md_generator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+filename=$(date +"%Y-%m-%d-.md")
+
+cat > "$filename" << EOF
+---
+layout: blog
+banana: true
+category: API_software
+title:  
+date:   $(date +"%Y-%m-%d %H:%M:%S")
+banner:
+  image: https://bit.ly/3xTmdUP
+tags:
+- API_software
+---
+
+* content
+{:toc}
+EOF
+
+echo "文件已生成：$filename"
